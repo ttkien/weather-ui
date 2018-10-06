a9a0e88569c20cc10ae9b608fd96bcf8c8f7cb3d
Kien Tran <kientt10@tcbs.com.vn>
2018-10-06 23:55:43 +0700

    first commit

---
+++ src/SearchLocationItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+var SearchLocationItem = (props) => {
+        return (
+            <View>
+                <Text>{props.name}</Text>
+            </View>
+        )
+}
+
+export default SearchLocationItem
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export SearchLocationItem from "./SearchLocationItem"
